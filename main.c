c9a676a96c99e2df42bc6041d0ba74a902bba0e9
KIMO892004 <kimoragab2004@gmail.com>
2023-06-22 08:06:02 -0700

    kimo

--- main.c
+++ main.c
@@ -1,1 +1,47 @@
-iiii
+#include "monty.h"
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+* main - monty code interpreter
+* @argc: the number of arguments
+* @argv: the monty file location
+* Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	char *cont;
+	FILE *file;
+	size_t s = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counters = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		cont = NULL;
+		read_line = getline(&cont, &s, file);
+		bus.cont = cont;
+		counters++;
+		if (read_line > 0)
+		{
+			execute(cont, &stack, counters, file);
+		}
+		free(cont);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
+}
