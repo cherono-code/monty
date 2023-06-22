f793af8d6e925727c7ab229330823f742a1cac3d
KIMO892004 <kimoragab2004@gmail.com>
2023-06-22 08:20:43 -0700

    kimo

---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * f_pop - printing the top
+ * @head: the stack head
+ * @counter: the line_number
+ * Return: no return
+*/
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *r;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	r = *head;
+	*head = r->next;
+	free(r);
+}
