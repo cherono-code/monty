244f6ac08b2ddde2798a51f949220db2013fc745
cherono-code <cheronodaisy38@gmail.com>
2023-06-22 09:14:56 -0700

    main

--- main.c
+++ main.c
@@ -1,47 +1,49 @@
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
-* main - monty code interpreter
-* @argc: the number of arguments
-* @argv: the monty file location
-* Return: 0 on success
-*/
-
+ * main - Monty code interpreter
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-	char *cont;
+	char *content = NULL;
 	FILE *file;
-	size_t s = 0;
+	size_t size = 0;
 	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-	unsigned int counters = 0;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	while (read_line > 0)
 	{
-		cont = NULL;
-		read_line = getline(&cont, &s, file);
-		bus.cont = cont;
-		counters++;
+		read_line = getline(&content, &size, file);
+		counter++;
+
 		if (read_line > 0)
 		{
-			execute(cont, &stack, counters, file);
+			content[strcspn(content, "\n")] = '\0';
+			execute(content, &stack, counter, file);
 		}
-		free(cont);
 	}
+
+	free(content);
 	free_stack(stack);
 	fclose(file);
-return (0);
+
+	return (0);
 }
+
